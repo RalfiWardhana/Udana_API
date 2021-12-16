72f87e4acd952ab275525a55655be5ce9f42319b
RalfiWardhana <ralfigeophysics@gmail.com>
2021-12-16 09:25:25 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
 import logo from './logo.svg';
 import './App.css';
+import{Table,Container,Navbar} from 'react-bootstrap'
+import { useEffect, useState } from 'react';
+import axios from "axios";
 
 function App() {
+  const [data,setData] =useState([])
+  const getData = async () => {
+    const datas = await axios.get("https://api.openbrewerydb.org/breweries")
+    console.log(datas.data)
+    setData(datas.data)   
+  }
+  useEffect(()=>{
+    getData()
+},[])
+let i = 1
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  
+      <Navbar expand="lg" bg="dark" variant="dark">
+        <Container>
+          <Navbar.Brand>API Open Brewery db</Navbar.Brand>
+        </Container>
+      </Navbar>
+    
+      <Container className='mt-3'>
+        <Table striped hover responsive>
+          <thead>
+            <tr>
+              <th>No</th>
+              <th>Name</th>
+              <th>City</th>
+              <th>State</th>
+              <th>Postal Code</th>
+              <th>Country</th>
+            </tr>
+          </thead>
+          <tbody>
+            {data.map((dat)=>(
+            <tr>
+              <td>{i++}</td>
+              <td>{dat.name}</td>
+              <td>{dat.city}</td>
+              <td>{dat.state}</td>
+              <td>{dat.postal_code}</td>
+              <td>{dat.country}</td>
+            </tr>
+            ))}
+          </tbody>
+        </Table>
+      </Container>
     </div>
   );
 }
